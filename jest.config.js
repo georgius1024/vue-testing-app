8b89ba4983037dd9915bea49fd6c55683945d64d
Timofeev Yurii <georgius@ural.ru>
2020-08-09 18:51:53 +0500

    fix unit testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/e2e/'],
   collectCoverage: true
 }
